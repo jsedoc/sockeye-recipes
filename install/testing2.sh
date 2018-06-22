57f056826dd9a23c796ddc4e5af10fcf7555aa7d
jsedoc <jsedoc@users.noreply.github.com>
2018-06-22 16:41:59 -0400

    Create testing2.sh

---
+++ install/testing2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+SOCKEYE_COMMIT=5873da5bd640f862646ae59cf408ddcee1449777 # 1.18.15 (sockeye:master)
+
+# Get this version of sockeye
+rootdir="$(readlink -f "$(dirname "$0")/../")"
+cd $rootdir
+git submodule init
+git submodule update --recursive --remote sockeye
+cd sockeye
+git checkout $SOCKEYE_COMMIT
+
+$rootdir/install/testing.sh -s $rootdir/sockeye -e sockeye_gpu
